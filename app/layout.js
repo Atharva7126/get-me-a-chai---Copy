a93bf8ea5822ed96a45400b35687222aee7694af
Atharva Raut <atharvaraut7126@gmail.com>
2025-05-30 10:38:46 +0530

    forteenth commit 2

--- app/layout.js
+++ app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <SessionWarpper>
+        <SessionWarpper className="flex flex-col min-h-full">
           <Navbar />
           <div className="flex-grow bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px] text-white">
             {children}
